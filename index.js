17b85028e2ccf1cb3f80d1e5ecd76e63606c6a91
LazarMarianVasile <82505870+LazarMarianVasile@users.noreply.github.com>
2021-06-23 14:37:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-let randomNumber = Math.floor(Math.random() * 3);
-function randomWinnerFirstButton() {
-	if (randomNumber == 0) {
-		document.getElementById("firstButton").innerHTML = "You win!";
+function randomWinner(idButton) {
+	let randomNumber = Math.floor(Math.random() * 3);
+	if (randomNumber == idButton) {
+		document.getElementById("text").innerHTML = "You win!";
 	} else {
-		document.getElementById("firstButton").innerHTML = "Try again!";
-	}
-}
-function randomWinnerSecondButton() {
-	if (randomNumber == 1) {
-		document.getElementById("secondButton").innerHTML = "You win!";
-	} else {
-		document.getElementById("secondButton").innerHTML = "Try again!";
-	}
-}
-function randomWinnerThirdButton() {
-	if (randomNumber == 2) {
-		document.getElementById("thirdButton").innerHTML = "You win!";
-	} else {
-		document.getElementById("thirdButton").innerHTML = "Try again!";
+		document.getElementById("text").innerHTML = "Try again!<br/>The winner is Button " + randomNumber ;
 	}
 }
